fb7d844e27e47769cbadf967c70fcd75eec77852
luis montero <luis>
2024-03-08 23:49:30 -0300

    estilos y detalles

--- Desafios JavaScript/Nota_Finales/assets/js/script.js
+++ Desafios JavaScript/Nota_Finales/assets/js/script.js
@@ -7,9 +7,9 @@ var Nota3 = document.getElementById("Nota3");
 nombre.innerHTML = prompt("Ingrese el nombre");
 carrera.innerHTML = prompt("Ingrese la carrera");
 
-let NotaHtml1 = parseFloat(prompt("Ingrese la Nota 1"));
-let NotaHtml2 = parseFloat(prompt("Ingrese la Nota 2"));
-let NotaHtml3 = parseFloat(prompt("Ingrese la Nota 3"));
+let NotaHtml1 = parseFloat(prompt("Ingrese la Nota 1 de HTML"));
+let NotaHtml2 = parseFloat(prompt("Ingrese la Nota 2 de HTML"));
+let NotaHtml3 = parseFloat(prompt("Ingrese la Nota 3 de HTML"));
 
 let promedio =  (NotaHtml1 + NotaHtml2 + NotaHtml3) / 3;
 
@@ -18,9 +18,9 @@ Nota2.innerHTML = NotaHtml2;
 Nota3.innerHTML = NotaHtml3;
 Promedio1.innerHTML = promedio;
 
-let NotaCss1 = parseFloat(prompt("Ingrese la Nota 1"));
-let NotaCss2 = parseFloat(prompt("Ingrese la Nota 2"));
-let NotaCss3 = parseFloat(prompt("Ingrese la Nota 3"));
+let NotaCss1 = parseFloat(prompt("Ingrese la Nota 1 de CSS"));
+let NotaCss2 = parseFloat(prompt("Ingrese la Nota 2 de CSS"));
+let NotaCss3 = parseFloat(prompt("Ingrese la Nota 3 de CSS"));
 
 let promedioC2 =  (NotaCss1 + NotaCss2 + NotaCss3) / 3;
 
@@ -29,9 +29,9 @@ Nota5.innerHTML = NotaCss2;
 Nota6.innerHTML = NotaCss3;
 Promedio2.innerHTML = promedioC2;
 
-let NotaJs1 = parseFloat(prompt("Ingrese la Nota 1"));
-let NotaJs2 = parseFloat(prompt("Ingrese la Nota 2"));
-let NotaJs3 = parseFloat(prompt("Ingrese la Nota 3"));
+let NotaJs1 = parseFloat(prompt("Ingrese la Nota 1 de JavaScript"));
+let NotaJs2 = parseFloat(prompt("Ingrese la Nota 2 de JavaScript"));
+let NotaJs3 = parseFloat(prompt("Ingrese la Nota 3 de JavaScript"));
 
 let promedioJs3 =  (NotaJs1 + NotaJs2 + NotaJs3) / 3;
 
