bb5363d6d5c86da7d86adcd3c5bc1dd285043807
luis <luisjosemon@gmail.com>
2024-03-19 19:36:25 -0300

    detalle de clase estilo

--- Viaje_chile/assets/js/javascrip.js
+++ Viaje_chile/assets/js/javascrip.js
@@ -1,44 +1,52 @@
+/*  Contenido de Sección de Cambio de Estilo */
+
 $('#butonC').click(function () {
 
-    $('body').css('background-color', 'white');
+    $('body').css('background-color', 'seashell');
     $('body').css('color', 'navy');
+    $('.colort').css('color', 'black');
+
 });
 $('#butonO').click(function () {
 
     $('body').css('background-color', 'black');
     $('body').css('color', 'white');
+    $('.colort').css('color', 'white');
 });
 
-$('.i1').click(function(){
+/*  Contenido de Sección de las Cartas */
+
+$('.i1').click(function () {
     $('.i2, .i3, .i4').hide("slow");
 });
 $('.i1').dblclick(function () {
     $('.i2, .i3, .i4').show("slow");
 });
 
-$('.i2').click(function(){
+$('.i2').click(function () {
     $('.i1, .i3, .i4').hide("slow");
 });
 $('.i2').dblclick(function () {
     $('.i1, .i3, .i4').show("slow");
 });
 
-$('.i3').click(function(){
+$('.i3').click(function () {
     $('.i2, .i1, .i4').hide("slow");
 });
 $('.i3').dblclick(function () {
     $('.i2, .i1, .i4').show("slow");
 });
 
-$('.i4').click(function(){
+$('.i4').click(function () {
     $('.i2, .i3, .i1').hide("slow");
 });
 $('.i4').dblclick(function () {
     $('.i2, .i3, .i1').show("slow");
 });
 
+/*  Contenido de Sección de Formulario */
 
-$('.formula').click(function(){
+$('.formula').click(function () {
     $('.form-control').hide("slow");
 });
 
